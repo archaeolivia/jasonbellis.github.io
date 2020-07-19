e6f7517f3728f523ad56d4906373ac2874bb5f0c
Jason Bellis <jason.m.bellis@gmail.com>
2020-07-19 15:15:52 -0400

    portfolio class not created
    new instance of the Portfolio class firing now,..

--- js/app.js
+++ js/app.js
@@ -3,4 +3,4 @@ let app;
 import Portfolio from './classes/portfolio.js';
 
 //load the portfolio class into the app
-if (!app.Portfolio) window.Portfolio = Portfolio();
+if (!app.Portfolio) window.Portfolio = new Portfolio();
