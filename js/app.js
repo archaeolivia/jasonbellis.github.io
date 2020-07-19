bcf552724808107de845b9b54a8f28398056ea30
Jason Bellis <jason.m.bellis@gmail.com>
2020-07-19 14:59:42 -0400

    Attempting a fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let app;
 
-import Portfolio from '/classes/portfolio';
+import Portfolio from './classes/portfolio';
 
 //load the portfolio class into the app
 if (!app.Portfolio) window.Portfolio = Portfolio();
+Portfolio();
